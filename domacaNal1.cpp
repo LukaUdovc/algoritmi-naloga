c57f37cb92182cac8b5f5dd2e053fc7077bf47b6
LukaUdovc <luka.udovc@student.um.si>
2025-03-28 23:20:35 +0100

    ogrodje naloge --funkcije main, Branje_Stevil, Izpis_Stevil,Binarni_Radix_Sort +build files

--- domacaNal1.cpp
+++ domacaNal1.cpp
@@ -1,1 +1,23 @@
 //Ustvarjena nal. Algoritmi
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+bool Branje_Stevil(vector<unsigned char>& vec, const char s[]) {
+
+}
+
+void Izpis_Stevil(int* polje, unsigned int velikost) {
+
+}
+
+void Binarni_Radix_Sort(vector<unsigned char>& A) {
+
+}
+
+int main(int argc, const char* argv[]) {
+
+}
