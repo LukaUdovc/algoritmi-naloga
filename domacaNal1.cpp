65be675a2e0e39133a7aff4b9f44573a9bc6a9df
LukaUdovc <luka.udovc@student.um.si>
2025-03-29 02:24:18 +0100

    funkcija Binarni_Radix_Sort

--- domacaNal1.cpp
+++ domacaNal1.cpp
@@ -15,7 +15,32 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 }
 
 void Binarni_Radix_Sort(vector<unsigned char>& A) {
+    const int n = A.size();
 
+    for (int bit = 0; bit < 8; ++bit) {
+        vector<int> D(n);
+        for (int i = 0; i < n; ++i) {
+            D[i] = (A[i] >> bit) & 1;
+        }
+
+        vector<int> count(2, 0);
+        for (int b : D) count[b]++;
+
+        count[1] = count[0];
+        count[0] = 0;
+
+        vector<unsigned char> A_sorted(n);
+        vector<int> D_sorted(n);
+
+        for (int i = 0; i < n; ++i) {
+            int b = D[i];
+            int pos = count[b]++;
+            A_sorted[pos] = A[i];
+            D_sorted[pos] = b;
+        }
+
+        A = A_sorted;
+    }
 }
 
 int main(int argc, const char* argv[]) {
