f6579fcdb93a5123d3fe2d6e6ad5012fd9d227a4
LukaUdovc <luka.udovc@student.um.si>
2025-03-29 02:30:16 +0100

    implementacija funkcije Binarni_Radix_Sort v main

--- domacaNal1.cpp
+++ domacaNal1.cpp
@@ -14,23 +14,26 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 
 }
 
+// Binarni radix sort
 void Binarni_Radix_Sort(vector<unsigned char>& A) {
     const int n = A.size();
 
     for (int bit = 0; bit < 8; ++bit) {
+        // Polje D – bite trenutnega bita vseh števil
         vector<int> D(n);
         for (int i = 0; i < n; ++i) {
             D[i] = (A[i] >> bit) & 1;
         }
-
+        // Stable counting sort nad D – z upoštevanjem indeksa A
         vector<int> count(2, 0);
         for (int b : D) count[b]++;
 
+        // kumulativna vsota
         count[1] = count[0];
         count[0] = 0;
 
         vector<unsigned char> A_sorted(n);
-        vector<int> D_sorted(n);
+        vector<int> D_sorted(n);// ni nujno
 
         for (int i = 0; i < n; ++i) {
             int b = D[i];
@@ -38,7 +41,7 @@ void Binarni_Radix_Sort(vector<unsigned char>& A) {
             A_sorted[pos] = A[i];
             D_sorted[pos] = b;
         }
-
+        // Posodobi A
         A = A_sorted;
     }
 }
@@ -48,7 +51,11 @@ int main(int argc, const char* argv[]) {
         cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>\n";
         return 1;
     }
-
+    vector<unsigned char> A;
+    if (!Branje_Stevil(A, argv[1])) {
+        return 1;
+    }
+    Binarni_Radix_Sort(A);
     cout << "Sortiranje koncano. Rezultat v out.txt\n";
     return 0;
 }
