c1f6b8ba94c8a3de46b2b2086caf7d5e06a64b19
LukaUdovc <luka.udovc@student.um.si>
2025-03-29 01:55:36 +0100

    funkcija Branje_Stevil

--- domacaNal1.cpp
+++ domacaNal1.cpp
@@ -7,7 +7,25 @@
 using namespace std;
 
 bool Branje_Stevil(vector<unsigned char>& vec, const char s[]) {
+    ifstream input(s);
+    int st;
 
+    if (!input.is_open()) {
+        cerr << "Napaka: ne morem odpreti datoteke " << s << endl;
+        return false;
+    }
+
+    while (input >> st) {
+        if (st < 0 || st > 255) {
+            cerr << "Napaka: stevilo " << st << " ni v razponu [0, 255].\n";
+            return false;
+        }
+        vec.push_back(static_cast<unsigned char>(st));
+        while (isspace(input.peek())) input.get();
+    }
+
+    input.close();
+    return true;
 }
 
 void Izpis_Stevil(int* polje, unsigned int velikost) {
